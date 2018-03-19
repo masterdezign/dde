1cf2ee2dd97a3b98de4bc066dff749fe095bf3fe
Bogdan <masterdezign@gmail.com>
2018-03-19 15:53:39 +0100

    Reimplement core data types based on free vector spaces

--- dde/Numeric/DDE/Model.hs
+++ dde/Numeric/DDE/Model.hs
@@ -1,34 +1,41 @@
 {- |
    = Example DDE models
 
-   These example models are defined by `rhs` function.
-   Adding a new example model requires adjusting `rhs` and  `Par`.
-   Switching between models is performed by changing `Par`.
 -}
 module Numeric.DDE.Model (
-    Par (..)
-  , BandpassFiltering (..)
-  , rhs
+  BandpassFiltering (..)
+  , RC (..)
+  , MackeyGlass (..)
+  , mackeyGlassRhs
+  , bandpassRhs
   ) where
 
+import           Control.Lens
+import           Linear ( R1 (..)
+                        , R2 (..) )
+import qualified Linear.V1
+import qualified Linear.V2
 import qualified Data.Vector.Storable as V
 
 import           Numeric.DDE.Types
 
 
--- | Model parameters
-data Par =
-         -- | Mackey-Glass model (no external input)
+-- | Mackey-Glass model (no external input) parameters
+data MackeyGlass =
          MackeyGlass { _beta :: Double
                        , _gamma :: Double
                        }
-         |
 
-         --  | Bandpass-filtered two-dimensional nonlinear system with
-         --  external input @u(t)@.
-         --
-         --     \tau * dx(t)/dt = -x - y / theta + fnl[x(t - \tau_D) + \rho*u(t)]
-         --            dy(y)/dt = x
+--  | Bandpass-filtered two-dimensional nonlinear system with
+--  external input @u(t)@.
+--
+-- \[
+-- \begin{aligned}
+--   \tau * dx(t)/dt &= -x - y / theta + fnl[x(t - \tau_D) + \rho*u(t)] \\
+--          dy(y)/dt &= x
+-- \end{aligned}
+-- \]
+data RC =
          RC { _fnl :: Double -> Double  -- ^ Nonlinear transformation
             , _rho :: Double  -- ^ External forcing weight
             , _filt :: BandpassFiltering
@@ -40,27 +47,28 @@ data BandpassFiltering = BandpassFiltering
   , _theta :: Double  -- ^ Integration time, s  (delta = \tau_D / theta)
   } deriving Show
 
--- | DDE right-hand sides for example models
-rhs :: Par -> RHS
-
--- Mackey-Glass model with no external forcing
-rhs MackeyGlass { _beta = beta, _gamma = gamma }
-    ((State xs), (Hist hs), _)
-      = State (V.singleton x')
-        where x' = beta * x_tau / (1 + x_tau^(10::Int)) - gamma * x
-              x = xs V.! 0
-              x_tau = hs V.! 0
+-- | Mackey-Glass model with no external forcing
+mackeyGlassRhs :: MackeyGlass -> RHS (Linear.V1.V1 Double)
+mackeyGlassRhs MackeyGlass { _beta = beta, _gamma = gamma } = RHS _f
+  where
+    _f (xs, (Hist hs), _) = Linear.V1.V1 x'
+      where
+        x' = beta * x_tau / (1 + x_tau^(10::Int)) - gamma * x
+        x = xs ^._x
+        x_tau = hs ^._x
 
 -- Ikeda-like model with an integral term y(t) and external input
-rhs RC { _fnl = _fnl,
-         _rho = _rho,
-         _filt = BandpassFiltering { _tau = _tau, _theta = _theta }
-       }
-       ((State xs), (Hist hs), (Inp u)) = State $ V.fromList [x', y']
-         where
-           x' = (-x - (recip _theta) * y + _fnl (x_tau + _rho * u)) / _tau
-           y' = x  -- Integral term
+bandpassRhs :: RC -> RHS (Linear.V2.V2 Double)
+bandpassRhs RC { _fnl = _fnl,
+                 _rho = _rho,
+                 _filt = BandpassFiltering { _tau = tau, _theta = theta }
+               } = RHS _f
+ where
+   _f (xs, (Hist hs), (Inp u)) = Linear.V2.V2 x' y'
+     where
+       x' = (-x - (recip theta) * y + _fnl (x_tau + _rho * u)) / tau
+       y' = x  -- Integral term
 
-           x = xs V.! 0
-           y = xs V.! 1
-           x_tau = hs V.! 0  -- Delay term
+       x = xs ^._x
+       y = xs ^._y
+       x_tau = hs ^._x  -- Delay term
--- examples/MackeyGlass/Main.hs
+++ examples/MackeyGlass/Main.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
+import           Linear.V1
 import           Numeric.DDE
 import           Numeric.DDE.Model
 import qualified Data.Vector.Storable as V
 
-parMG0 :: Par
+parMG0 :: MackeyGlass
 parMG0 = MackeyGlass { _beta = 0.2
                      , _gamma = 0.1
                      }
@@ -12,18 +14,18 @@ parMG0 = MackeyGlass { _beta = 0.2
 -- | The Mackey-Glass model (with no external input).
 -- This example demonstrates how to set custom initial conditions.
 -- Typical hStep = 0.1
-mgModel :: Double -> Int -> V.Vector Double
+mgModel :: Double -> Int -> V.Vector (V1 Double)
 mgModel hStep totalIters = r
   where
     -- Initial state x(t0) = 0.2
-    state0 = State (V.replicate 1 0.2)
+    state0 = V1 0.2
     len1 = 17 * (round $ recip hStep)  -- tauD = 17, delay time
     -- Initial conditions
-    hist0 = V.replicate len1 0.2
+    hist0 = V.replicate len1 state0
     inp = Input (V.replicate (totalIters + 1) 0.0)
-    rhs' = rhs parMG0
+    rhs' = mackeyGlassRhs parMG0
     -- Stepper implements Runge-Kutta schema
-    stepper = let (Stepper1 _rk4) = rk4
+    stepper = let (Stepper _rk4) = rk4
               in _rk4 hStep rhs'
     -- Record all the time trace
     (_, r) = integ' stepper len1 totalIters totalIters (state0, hist0, inp)
@@ -54,7 +56,8 @@ runTest :: V.Vector Double
 runTest =
   let hStep = 0.1
       total = round(1000 / hStep)  -- 1000 time units
-  in mgModel hStep total
+      trace = mgModel hStep total
+  in V.map (\(V1 x) -> x) trace
 
 main :: IO ()
 main = putStrLn $ toString runTest
