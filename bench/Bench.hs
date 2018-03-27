073dcd469157b21fbd3604f5434e7025ef3269b2
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2018-03-27 22:51:19 +0200

    2D benchmarks with fast old comparison to reach in performance.

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,27 +1,19 @@
 import           Criterion.Main
-import           Linear.V1 ( V1 (..) )
+import           Linear.V3 ( V3 (..) )
 import qualified Data.Vector.Storable as V
 
-import qualified Impl1
-import qualified Impl2
+import qualified R1
+import qualified R2
 
-runTest1 :: Double -> Double
-runTest1 maxTime =
-  let hStep = 0.1
-      total = round(maxTime / hStep)
-      s = Impl1.mgModel hStep total
-  in s
+runTest1 :: Int -> V.Vector (V3 Double)
+runTest1 = R1.model
 
-runTest2 :: Double -> Double
-runTest2 maxTime =
-  let hStep = 0.1
-      total = round(maxTime / hStep)
-      (V1 s, _) = Impl2.mgModel hStep total
-  in s
+runTest2 :: Int -> V.Vector (V3 Double)
+runTest2 = R2.model
 
 main = defaultMain [
     bench "old version" $ whnf runTest1 maxTime
     , bench "target to optimize" $ whnf runTest2 maxTime
   ]
   where
-    maxTime = 1000  -- 1000 time units
+    maxTime = 20  -- No of delays
